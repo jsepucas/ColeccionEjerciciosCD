1f4fab1772a99c22e6174f0b099374191f3911fb
jsepucas <jsepucas@myuax.com>
2023-11-28 10:44:48 +0100

    1

---
+++ 1_Ejercicio.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> numeros;
+
+    numeros.push_back (15);
+    numeros.push_back (20);
+    numeros.push_back (25);
