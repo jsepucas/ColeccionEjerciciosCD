daef9099d6a8fa045beae746ef867010b1870cda
jsepucas <jsepucas@myuax.com>
2023-11-29 21:50:14 +0100

    2

--- 2_Ejercicio.cpp
+++ 2_Ejercicio.cpp
@@ -3,5 +3,11 @@
 #include <stack>
 
 int main () {
+    std::list<double> decimalList;
 
+    decimalList.push_back (3.1416);
+    decimalList.push_back (2.7182);
+    decimalList.push_back (1.6180);
+
+    std::stack<double, std::list<double> > decimalStack (decimalList);
 }
