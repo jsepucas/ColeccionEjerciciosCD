f0facb339d5644948185dd2143a5eb3bba55b90f
jsepucas <jsepucas@myuax.com>
2023-11-30 16:30:32 +0100

    8

--- 3_Ejercicio.cpp
+++ 3_Ejercicio.cpp
@@ -4,3 +4,8 @@
 
 using namespace std;
 
+int main() {
+    vector<int> numeros = {3, 10, 14, 16, 20, 23, 25, 29}
+    sort(numeros.begin(), numeros.end());
+    cout<< "Numeros ordenados: " << endl;
+}
