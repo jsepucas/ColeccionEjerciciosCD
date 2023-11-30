9d2c18559cf1e5286d429db598f7ed07b0a16371
jsepucas <jsepucas@myuax.com>
2023-11-30 17:37:29 +0100

    Ultimo :/

--- 6_Librerias.cpp
+++ 6_Librerias.cpp
@@ -3,3 +3,13 @@
 
 int main() {
     std::string s = "12345";
+
+    try {
+        int i = boost::lexical_cast<int>(s);
+        std::cout << "Número: " << i << std::endl;
+    } catch(boost::bad_lexical_cast &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
