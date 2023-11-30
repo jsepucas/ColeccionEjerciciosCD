994b9890b8a1a1ccb324352e6e8cf617b5fc35bc
jsepucas <jsepucas@myuax.com>
2023-11-30 16:54:26 +0100

    13

--- 4_Ejercicio.cpp
+++ 4_Ejercicio.cpp
@@ -3,6 +3,9 @@
 #include <cmath>
 
 using namespace std;
+
+int main() {
+
  valarray<double> nums = {2.9, 3.0, 3.1, 3.2, 4.1};
 
  valarray<double> suma = nums + 10.0;
@@ -10,3 +13,12 @@ using namespace std;
  valarray<double> multiplicacion = nums * 2.0;
  valarray<double> division = nums / 2.0;
  valarray<double> raizCuadrada = sqrt(nums);
+
+
+ cout << "Numeros iniciales: ";
+ for (double num : nums) cout << num << " ";
+ cout << endl;
+
+ cout << "Suma: ";
+ for (double num : suma) cout << num << " ";
+ cout << endl;
