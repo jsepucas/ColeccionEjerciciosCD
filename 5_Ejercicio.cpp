82778dc8d29fd2fc3cf8ed97732d92ff6104326a
jsepucas <jsepucas@myuax.com>
2023-11-30 17:07:08 +0100

    18

--- 5_Ejercicio.cpp
+++ 5_Ejercicio.cpp
@@ -2,3 +2,10 @@
 #include <vector>
 #include <algorithm>
 #include <memory>
+
+using namespace std;
+
+int main() {
+    auto square = [](int n) { return n * n; };
+    vector<int> numbers = {1, 2, 3, 4, 5};
+
