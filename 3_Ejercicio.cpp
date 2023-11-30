c8811a435fb147679948ff106a0e5f74967a3861
jsepucas <jsepucas@myuax.com>
2023-11-30 16:49:10 +0100

    12

--- 3_Ejercicio.cpp
+++ 3_Ejercicio.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-    vector<int> numeros = {3, 10, 14, 16, 20, 23, 25, 29}
+    vector<int> numeros = {3, 10, 14, 16, 20, 23, 25, 29, 17};
     sort(numeros.begin(), numeros.end());
     cout<< "Numeros ordenados: " << endl;
 
@@ -17,4 +17,6 @@ int main() {
     cout << "El numero mas alto es: " << *maxElement << endl;
     auto minElemento = min_element(numeros.begin(), numeros.end());
     cout << "El numero mas bajo es: " << *minElemento << endl;
+
+    return 0;
 }
---
+++ 4_Ejercicio.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <valarray>
+#include <cmath>
+
+using namespace std;
+ valarray<double> nums = {2.9, 3.0, 3.1, 3.2, 4.1};
+
+ valarray<double> suma = nums + 10.0;
+ valarray<double> resta = nums - 5.0;
+ valarray<double> multiplicacion = nums * 2.0;
+ valarray<double> division = nums / 2.0;
+ valarray<double> raizCuadrada = sqrt(nums);
